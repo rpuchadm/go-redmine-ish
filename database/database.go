33d7dcb3f298bf3511847f018ac913f4630c0f4c
Rafael Puchades <rpuchadm@cfp.upv.es>
2025-03-22 21:51:58 +0100

    otro

--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/lib/pq" // Driver de PostgreSQL
 )
 
-func InitDB(cfg *config.Config) error {
+func InitDB(cfg *config.Config) (*sql.DB, error) {
 	// Cadena de conexión a PostgreSQL
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
@@ -17,16 +17,16 @@ func InitDB(cfg *config.Config) error {
 	// Conectar a la base de datos
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		return fmt.Errorf("error al conectar a la base de datos: %v", err)
+		return nil, fmt.Errorf("error al conectar a la base de datos: %v", err)
 	}
 
 	// Verificar la conexión
 	err = db.Ping()
 	if err != nil {
-		return fmt.Errorf("error al verificar la conexión: %v", err)
+		return nil, fmt.Errorf("error al verificar la conexión: %v", err)
 	}
 
 	// log.Println("Conexión a PostgreSQL establecida")
 
-	return nil
+	return db, nil
 }
